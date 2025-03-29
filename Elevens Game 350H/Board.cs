be0e1b837476a7074fde5170e1fc32b1733453ce
Mouad Oulouali <mouadoune430@gmail.com>
2025-03-29 14:23:44 -0400

    added elevens game constructor and var

--- Elevens Game 350H/Board.cs
+++ Elevens Game 350H/Board.cs
@@ -1,14 +1,51 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
-public class Board {
-    private int Rank rank;
-    private int Suit suit;
+public class Board
+{
 
-    public Board(int rank, int suit) {
-        this.Rank = rank;
-        this.Suit = suit;
+    // array of cards
+    private Card[] cards;
+
+    // list of selected cards
+    private List<int> selectedCards;
+
+    // constructor
+    public Board()
+    {
+        cards = new Card[9]; // array of 9 cards for the board.
+        selectedCards = new List<int>(); // to add each selected card to.
     }
 
+    // replacing card at specific position
+    public void replaceSelectedCard(int position, Card newCard)
+    {
+        // error checking
+        if (position >= 0 && position < cards.Length)
+        {
+            cards[position] = newCard;
+        }
+    }
+
+    // checking if selected cards add up to 11
+    public bool checkSum()
+    {
+        if (selectedCards.Count < 2 || selectedCards.Count > 2)
+        {
+            return false;
+        } 
+        int sum = 0;
+
+        foreach (int position in selectedCards) {
+            // checking if not null for when cards are deleted
+            if (position >= 0 && position < cards.Length && cards[position] != null){
+                sum += cards[position].GetValue();
+            }
+        }
+
+        return sum == 11;
+    }
 
 }
--- Elevens Game 350H/ElevensGame.cs
+++ Elevens Game 350H/ElevensGame.cs
@@ -1,4 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+public class ElevensGame
+{
+    private Deck deck;
+    private Board board;
+    private int loss_count;
+    private int win_count;
 
-using namespace ElevensGame;
+    // constructor to initialize wins and losses
+    public ElevensGame()
+    {
+        win_count = 0;
+        loss_count = 0;
+    }
+    public void selectedCard() {
+        Card selectedCard = board.Select(position);
+    }
+
+}
 
